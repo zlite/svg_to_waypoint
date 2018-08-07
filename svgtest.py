4dffaffe8a57d066d2a46faa863c2018bdd409fd
Chris Anderson <gca2@earthlink.net>
2018-08-07 07:54:51 -0700

    Got csv writing/reading working

--- svgtest.py
+++ svgtest.py
@@ -9,34 +9,27 @@ d = attributes[1]['d']  # d-string from first path in SVG
 # Now for some regular expressions magic
 print('Start')
 split_by_letters = re.findall('[A-Z|a-z][^A-Z|a-z]*', d)
-data = []
 waypoint = []
 for x in split_by_letters:
     nums = x[1:].replace(',',' ').split()  # list of numbers after letter
-    for k in range(len(nums) // 2):
-        data.append([x[0]] +  [nums[k]] + [nums[k+1]])
-        if x[0] == 'M':
-            print(x[0])
-            print (nums[k],nums[k+1])
-            waypoint.append('M')
-            waypoint.append(nums[k])
-            waypoint.append(nums[k+1])
-            waypoint.append('L')
-            print ('L')
-        if x[0] == 'L':
-            print (nums[k],nums[k+1])
-            waypoint.append(nums[k])
-            waypoint.append(nums[k+1])
-myFile = open('waypoint.csv', 'w',newline='')
-with myFile:
-    writer = csv.writer(myFile)
-    writer.writerows(nums)
+    waypoint.append(nums)
+print('Waypoints: ', waypoint)
+
+with open('waypoint.csv', mode='w') as csv_file:
+    fieldnames = ['x', 'y']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    for k in range(len(waypoint)):
+        writer.writerow({'x':waypoint[k][0],'y': waypoint[k][1]})
+    
 print("Done with writing")
 # now let's read that file
 print('Reading....')
-with open('waypoint.csv', newline='') as csvfile:
-    file = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in file:
-        print(', '.join(row))
-print(row)         
+with open('waypoint.csv', mode='r') as csv_file:
+    line_count = 0
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        print(row['x'], row['y'])
+        line_count += 1
+    print('Processed lines:',line_count)      
 print('Done')
